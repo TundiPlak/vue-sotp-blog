882cc2735ae06f8d92de298dd16c62dd51c3213c
Tundi Torok <tunde.torok.pr@gmail.com>
2021-06-11 10:43:42 +0300

    refactor

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,55 +1,87 @@
 <template>
   <div class="blog-wrapper no-user">
     <div class="blog-content">
-      <div>
-        <h2 v-if="post.welcomeScreen">{{ post.title }}</h2>
-        <h2 v-else>{{ post.title }}</h2>
-        <p v-if="post.welcomeScreen">{{ post.blogPost }}</p>
+      <div class="content-wrapper">
+        <h2 class="content-header">{{ post.title }}</h2>
+        <p class="content-blog-post" v-if="post.welcomeScreen">
+          {{ post.blogPost }}
+        </p>
         <p class="content-preview" v-else>{{ post.blogHTML }}</p>
-        <router-link class="link link-light" v-if="post.welcomeScreen" to="#">
-          Login/Register<Arrow class="arrow arrow-light" />
-        </router-link>
-        <router-link class="link" v-else to="#">
-          View Post <Arrow class="arrow" />
-        </router-link>
+
+        <div :class="contentLink.classes" @click="contentLinkHandler">
+          {{ contentLink.text }}
+          <Arrow :class="contentLink.arrowClasses" />
+        </div>
       </div>
     </div>
+
     <div class="blog-photo">
-      <img
-        v-if="post.welcomeScreen"
-        :src="require(`../assets/blogPhotos/${post.photo}.jpg`)"
-        alt=""
-      />
-      <img
-        v-else
-        :src="require(`../assets/blogPhotos/${post.blogCoverPhoto}.jpg`)"
-        alt=""
-      />
+      <img :src="require(`../assets/blogPhotos/${blogPhoto}.jpg`)" alt="" />
     </div>
   </div>
 </template>
 
 <script>
 import Arrow from "../assets/Icons/arrow-right-light.svg";
+
 export default {
-  name: "blogPost",
-  props: ["post"],
+  name: "BlogPost",
   components: { Arrow },
+  props: {
+    post: {
+      type: Object,
+      default: null,
+    },
+  },
+  computed: {
+    blogPhoto() {
+      const { welcomeScreen } = this.post;
+      return this.post[welcomeScreen ? "photo" : "blogCoverPhoto"];
+    },
+    contentLink() {
+      const { welcomeScreen } = this.post;
+      const classes = `content-link${
+        welcomeScreen ? " content-link-light" : ""
+      }`;
+      const text = welcomeScreen ? "Login/Register" : "View Post";
+      const arrowClasses = `arrow${welcomeScreen ? " arrow-light" : ""}`;
+      return {
+        classes,
+        text,
+        arrowClasses,
+      };
+    },
+  },
+  methods: {
+    contentLinkHandler() {
+      console.log("clicked");
+      this.$router.push({ name: "Home" });
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-.blog-wrapper {
-  display: flex;
-  flex-direction: column;
-  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
-    0 2px 4px -1px rgba(0, 0, 0, 0.06);
-  @media (min-width: 700px) {
-    min-height: 650px;
-    max-height: 650px;
-    flex-direction: row;
+.blog {
+  &-wrapper {
+    display: flex;
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+      0 2px 4px -1px rgba(0, 0, 0, 0.06);
+    @media (min-width: 700px) {
+      min-height: 650px;
+      max-height: 650px;
+      flex-direction: row;
+    }
+    &:nth-child(even) {
+      .blog-content {
+        order: 2;
+      }
+      .blog-photo {
+        order: 1;
+      }
+    }
   }
-  .blog-content {
+  &-content {
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -62,53 +94,8 @@ export default {
     @media (min-width: 800px) {
       flex: 3;
     }
-    div {
-      max-width: 375px;
-      padding: 72px 24px;
-      @media (min-width: 700px) {
-        padding: 0 24px;
-      }
-      h2 {
-        font-size: 32px;
-        font-weight: 300;
-        text-transform: uppercase;
-        margin-bottom: 24px;
-        @media (min-width: 700px) {
-          font-size: 40px;
-        }
-      }
-      p {
-        font-size: 15px;
-        font-weight: 300;
-        line-height: 1.7;
-      }
-      .content-preview {
-        font-size: 13px;
-        max-height: 24px;
-        width: 250px;
-        white-space: nowrap;
-        overflow: hidden;
-        text-overflow: ellipsis;
-      }
-      .link {
-        display: inline-flex;
-        align-items: center;
-        margin-top: 32px;
-        padding-bottom: 4px;
-        border-bottom: 1px solid transparent;
-        transition: 0.5s ease-in all;
-        &:hover {
-          border-bottom-color: #303030;
-        }
-      }
-      .link-light {
-        &:hover {
-          border-bottom-color: #ffff;
-        }
-      }
-    }
   }
-  .blog-photo {
+  &-photo {
     order: 1;
     flex: 3;
     box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
@@ -126,12 +113,52 @@ export default {
       object-fit: cover;
     }
   }
-  &:nth-child(even) {
-    .blog-content {
-      order: 2;
+}
+
+.content {
+  &-wrapper {
+    max-width: 375px;
+    padding: 72px 24px;
+    @media (min-width: 700px) {
+      padding: 0 24px;
     }
-    .blog-photo {
-      order: 1;
+  }
+  &-header {
+    font-size: 32px;
+    font-weight: 300;
+    text-transform: uppercase;
+    margin-bottom: 24px;
+    @media (min-width: 700px) {
+      font-size: 40px;
+    }
+  }
+  &-blog-post,
+  &-preview {
+    font-size: 15px;
+    font-weight: 300;
+    line-height: 1.7;
+  }
+  &-preview {
+    font-size: 13px;
+    max-height: 24px;
+    width: 250px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  &-link {
+    cursor: pointer;
+    display: inline-flex;
+    align-items: center;
+    margin-top: 32px;
+    padding-bottom: 4px;
+    border-bottom: 1px solid transparent;
+    transition: 0.5s ease-in all;
+    &:hover {
+      border-bottom-color: #303030;
+    }
+    &-light:hover {
+      border-bottom-color: #ffff;
     }
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,15 +6,29 @@
       v-for="(post, index) in sampleBlogPost"
       :key="index"
     ></blog-post>
+    <div class="blog-card-wrap">
+      <div class="container">
+        <h3>View More Recent Blogs</h3>
+        <div class="blog-cards">
+          <blog-card
+            :post="post"
+            v-for="(post, index) in sampleBlogCards"
+            :key="index"
+          ></blog-card>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import BlogPost from "../components/BlogPost.vue";
+import BlogCard from "../components/BlogCard.vue";
 export default {
   name: "Home",
   components: {
     BlogPost,
+    BlogCard,
   },
   data() {
     return {
@@ -37,7 +51,71 @@ export default {
           blogCoverPhoto: "designed-for-everyone",
         },
       ],
+      sampleBlogCards: [
+        {
+          blogTitle: "Blog Card #1",
+          blogCoverPhoto: "stock-1",
+          blogDate: "June 9, 2021",
+        },
+        {
+          blogTitle: "Blog Card #2",
+          blogCoverPhoto: "stock-2",
+          blogDate: "June 9, 2021",
+        },
+        {
+          blogTitle: "Blog Card #3",
+          blogCoverPhoto: "stock-3",
+          blogDate: "June 9, 2021",
+        },
+        {
+          blogTitle: "Blog Card #4",
+          blogCoverPhoto: "stock-4",
+          blogDate: "June 9, 2021",
+        },
+      ],
     };
   },
 };
 </script>
+
+<style lang="sccs" scoped>
+  /*.blog-card-wrap {
+   h3 {
+    font-weight: 300;
+    font-size: 28px;
+    margin-bottom: 32px;
+  }
+}
+.updates {
+  .container {
+    padding: 100px 25px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    @media (min-width: 800px) {
+      padding: 125px 25px;
+      flex-direction: row;
+    }
+    .router-button {
+      display: flex;
+      font-size: 14px;
+      text-decoration: none;
+      @media (min-width: 800px) {
+        margin-left: auto;
+      }
+    }
+    h2 {
+      font-weight: 300;
+      font-size: 32px;
+      max-width: 425px;
+      width: 100%;
+      text-align: center;
+      text-transform: uppercase;
+      @media (min-width: 800px) {
+        text-align: initial;
+        font-size: 40px;
+      }
+    }
+  }
+} */
+</style>
