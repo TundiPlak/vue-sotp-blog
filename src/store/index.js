46ee7d3c72742d55955d5e7a5af0b0229bb0b175
Tundi Torok <tunde.torok.pr@gmail.com>
2021-06-15 10:20:19 +0300

    vuex exp

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,40 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import mutations from './mutations.js';
+import actions  from './actions.js';
+import getters  from './getters.js';
+
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  state() {
+    return {
+    sampleBlogCards: [
+      {
+        blogTitle: "Blog Card #1",
+        blogCoverPhoto: "stock-1",
+        blogDate: "June 9, 2021",
+      },
+      {
+        blogTitle: "Blog Card #2",
+        blogCoverPhoto: "stock-2",
+        blogDate: "June 9, 2021",
+      },
+      {
+        blogTitle: "Blog Card #3",
+        blogCoverPhoto: "stock-3",
+        blogDate: "June 9, 2021",
+      },
+      {
+        blogTitle: "Blog Card #4",
+        blogCoverPhoto: "stock-4",
+        blogDate: "June 9, 2021",
+      },
+    ]},
+    mutations,
+    getters,
+    actions
+ 
+}
+});
